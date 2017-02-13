337e4766d7d1c9eb96c1bcfe9a99b35acd329c6e
tswingleton <swingleton@gmail.com>
2017-02-12 23:05:32 -0800

    Styles

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
-      config.assets.enabled = false
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
